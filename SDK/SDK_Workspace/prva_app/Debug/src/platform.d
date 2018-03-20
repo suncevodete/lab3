6d965abe7ae5628318fd6c0134ce995598facc51
Jelena Boroja <jelena.boroja1@gmail.com>
2018-03-20 10:16:45 +0100

    zavrsen hello world

---
+++ SDK/SDK_Workspace/prva_app/Debug/src/platform.d
@@ -0,0 +1,22 @@
+src/platform.d: ../src/platform.c \
+ ../../prva_app_bsp/microblaze_0/include/xparameters.h \
+ ../../prva_app_bsp/microblaze_0/include/xil_cache.h \
+ ../../prva_app_bsp/microblaze_0/include/mb_interface.h \
+ ../../prva_app_bsp/microblaze_0/include/xbasic_types.h \
+ ../../prva_app_bsp/microblaze_0/include/xil_types.h \
+ ../../prva_app_bsp/microblaze_0/include/xparameters.h \
+ ../src/platform_config.h
+
+../../prva_app_bsp/microblaze_0/include/xparameters.h:
+
+../../prva_app_bsp/microblaze_0/include/xil_cache.h:
+
+../../prva_app_bsp/microblaze_0/include/mb_interface.h:
+
+../../prva_app_bsp/microblaze_0/include/xbasic_types.h:
+
+../../prva_app_bsp/microblaze_0/include/xil_types.h:
+
+../../prva_app_bsp/microblaze_0/include/xparameters.h:
+
+../src/platform_config.h:
